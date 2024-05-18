d431fce43ac41c90874b757a52e88290d4beac4f
abstxn <108669111+abstxn@users.noreply.github.com>
2024-05-19 07:20:52 +0800

    20 current  2024-05-19 07:20:48  24.05.20240517.4a6b83b   6.6.31                          *

--- flake.nix
+++ flake.nix
@@ -4,10 +4,10 @@ description = "Default configuration for Opus";
 
 inputs = {
 
-  nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   home-manager = {
-    url = "github:nix-community/home-manager/release-23.11";
+    url = "github:nix-community/home-manager";
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
