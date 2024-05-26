b11c27a9a68c24935eea157524c83e8cf633e419
abstxn <108669111+abstxn@users.noreply.github.com>
2024-05-26 09:44:29 +0800

    48 current  2024-05-26 09:44:27  24.05.20240517.4a6b83b  6.6.31                          *

--- hosts/common/optional/steam.nix
+++ hosts/common/optional/steam.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ... }: {
   programs = {
     steam = {
       enable = true;
@@ -6,4 +6,6 @@
     };
     gamemode.enable = true;
   };
+
+  environment.systemPackages = [ pkgs.mangohud ];
 }
